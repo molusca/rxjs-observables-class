77cf35ad874bb72bcaab520a0a67028129dfb914
Lucas Freitas <pro.lucasrfreitas@gmail.com>
2022-06-02 09:52:05 -0300

    create custom observer with error and completion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,8 +12,36 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.firstSubscription = interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.firstSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObservable = Observable.create((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+
+        // completing observable
+        if (count === 5) {
+          observer.complete();
+        }
+
+        // erroring observable
+        if (count > 5) {
+          observer.error(new Error('Count is greater than 5!'));
+        };
+
+        count++;
+      }, 1000)
+    });
+
+    customIntervalObservable.subscribe((data) => {
+      console.log(data);
+    }, error => {
+      console.log(error)
+      alert(error);
+    }, () => {
+      console.log('COMPLETED!!!');
     });
   }
 
