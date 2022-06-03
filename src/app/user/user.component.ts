dbc2c4a27dc08a9fbfe73d773a6b9666e526f6a7
Lucas Freitas <pro.lucasrfreitas@gmail.com>
2022-06-03 00:35:16 -0300

    integrate event based changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
@@ -8,13 +9,21 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   id: number;
+  activated = false;
 
-  constructor(private route: ActivatedRoute) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+  }
+
+  onActivate() {
+    this.activated = !this.activated;
+    this.userService.activatedEmitter.next(this.activated);
   }
 }
